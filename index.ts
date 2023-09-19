4d8e665e08dd00d45122beb35d6d093fdf004fae
ming <420642207@qq.com>
2023-09-19 18:49:01 +0800

    feat: init express

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
+import express = require("express");
+import Monitor from "./monitor/index";
 
 
-function main() {
-    console.log('main');
-}
+const monitor = new Monitor();
+monitor.start()
 
-main()
+
+const app: express.Application = express();
+
+
+app.get("/", (req, res) => {
+    res.write('<h1>Welcome!</h2>');
+    res.end();
+});
+
+
+app.listen(3000, () => {
+    console.log('server start at 3000')
+});
