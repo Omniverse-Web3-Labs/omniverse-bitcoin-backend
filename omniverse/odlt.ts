603d6b866c5e4c3f7c9b44b06c2e3ab1eb4fd723
virgil2019 <danten0402@gmail.com>
2024-04-23 00:10:55 +0800

    Update for zk-version state transition

--- omniverse/odlt.ts
+++ omniverse/odlt.ts
@@ -5,9 +5,9 @@
 import {ODLT} from '@hthuang/bitcoin-lib/dist';
 import {logger} from '../utils';
 
-export interface Block {
+export interface Batch {
     // batch id of omniverse
-    batchId: string;
+    batchId: number;
     // block height of bitcoin
     btcBlockHeight: string;
     // the tx id of bitcoin
@@ -16,17 +16,19 @@ export interface Block {
     index: number;
     // p2tr output where the batch is committed to
     receipt: string;
+    // token amount `receipt` received
+    value: number;
 }
 
 export interface CommittedBatch {
     // batch id of omniverse
-    batchId: string,
+    batchId: number,
     // p2tr output where the batch is committed to
     receipt: string,
 }
 
 export class ODLTRecord {
-    blocks: Array<Block> = new Array<Block>();
+    batches: Array<Batch> = new Array<Batch>();
     committedBatch: CommittedBatch | null;
 
     /**
@@ -34,7 +36,7 @@ export class ODLTRecord {
      */
     constructor(receipt: string) {
         this.committedBatch = {
-            batchId: '0',
+            batchId: 0,
             receipt: receipt
         };
     }
@@ -59,42 +61,43 @@ export class ODLTRecord {
             return;
         }
 
-        if (this.blocks.length == 0) {
-            this.pushNewBatch(blockHeight, tx.txid, vout.n, committedBatch.receipt);
+        if (this.batches.length == 0) {
+            this.pushNewBatch(blockHeight, tx.txid, vout.n, committedBatch.receipt, vout.value);
         }
         else {
-            let preBlock = this.blocks[this.blocks.length - 1];
+            let preBlock = this.batches[this.batches.length - 1];
             let vin = tx.vin.find((element: any) => element.txid == preBlock.txid && element.vout == preBlock.index);
             if (!vin) {
                 logger.warn(`receipt: ${committedBatch.receipt} of batch: ${committedBatch.batchId} can not match inputs`);
             }
             else {
-                this.pushNewBatch(blockHeight, tx.txid, vout.n, committedBatch.receipt);
+                this.pushNewBatch(blockHeight, tx.txid, vout.n, committedBatch.receipt, vout.value);
             }
         }
     }
 
-    pushNewBatch(blockHeight: string, txid: string, index: number, receipt: string) {
+    pushNewBatch(blockHeight: string, txid: string, index: number, receipt: string, value: number) {
         let committedBatch = this.committedBatch!;
-        this.blocks.push({
+        this.batches.push({
             batchId: committedBatch.batchId,
             btcBlockHeight: blockHeight,
             txid,
             index,
             receipt,
+            value,
         });
         this.committedBatch = null;
     }
 
-    getBlockByHeight(number: number): Block | undefined {
-        if (number < this.blocks.length) {
-            return this.blocks[number];
+    getBatch(id: number): Batch | undefined {
+        if (id < this.batches.length) {
+            return this.batches[id];
         }
 
         return undefined;
     }
 
-    commitReceipt(batchId: string, receipt: string): boolean {
+    commitReceipt(batchId: number, receipt: string): boolean {
         if (this.committedBatch?.batchId == batchId) {
             this.committedBatch = {
                 batchId,
--- rpc/index.ts
+++ rpc/index.ts
@@ -4,25 +4,25 @@
 
 
 import express from "express";
-import { ODLTRecord, Block } from '../omniverse/odlt';
+import { ODLTRecord, Batch } from '../omniverse/odlt';
 import cors from "cors";
 
 export default function(odlt: ODLTRecord, app: express.Application) {
     app.use(cors());
 
-    app.get("/api/getLatestBlockInfo", (req, res) => {
-        if (odlt.blocks.length == 0) {
+    app.get("/api/getLatestBatchInfo", (req, res) => {
+        if (odlt.batches.length == 0) {
             res.json(null);
         }
         else {
-            res.json(odlt.blocks[odlt.blocks.length - 1]);
+            res.json(odlt.batches[odlt.batches.length - 1]);
         }
     });
 
 
-    app.get("/api/getBlock", (req, res) => {
-        const height = req.query['height'] as string;
-        let ret = odlt.getBlockByHeight(parseInt(height));
+    app.get("/api/getBatch", (req, res) => {
+        const batchId = req.query['height'] as string;
+        let ret = odlt.getBatch(parseInt(batchId));
         if (ret) {
             res.json(ret);
         }
@@ -36,7 +36,7 @@ export default function(odlt: ODLTRecord, app: express.Application) {
     app.get("/api/commitBlockState", (req, res) => {
         const height = req.query['height'] as string;
         let receipt = req.query['receipt'] as string;
-        let ret = odlt.commitReceipt(height, receipt);
+        let ret = odlt.commitReceipt(parseInt(height), receipt);
         res.json({
             result: ret,
         });
